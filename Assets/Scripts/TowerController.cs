a9b49ee0aa391468302622f5d32be1e288c068b1
TrisThien <professor.no90@gmail.com>
2022-02-10 02:48:19 +0700

    update level design

--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -1,11 +1,12 @@
 using System;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 using Random = UnityEngine.Random;
 public class TowerController : MonoBehaviour
 {
     private float _rotateSpeed = 2.6f;
     
-    [SerializeField] private PlatformController[] platform;
+    [SerializeField] private PlatformController[] platforms;
     
     private float _platformHeight = 0.4f;
     private float _platformGap = 0.2f;
@@ -36,14 +37,14 @@ public class TowerController : MonoBehaviour
     }
     private void GenerateLevel()
     {
-        for (int i = 1; i < floorNumbers; i++)
+        for (var i = 1; i < floorNumbers * 1/3; i++)
         {
-            _floors[i] = new PlatformController[360 / platform[Random.Range(0, platform.Length)].angle];
-    
-            for (int angle = platform[Random.Range(0, platform.Length)].angle; angle <= 360;
-                angle += platform[Random.Range(0, platform.Length)].angle)
+            _floors[i] = new PlatformController[360 / platforms[0].angle];
+
+            for (int angle = platforms[0].angle; angle <= 360;
+                angle += platforms[0].angle)
             {
-                PlatformController p = Instantiate(platform[Random.Range(0, platform.Length)], 
+                PlatformController p = Instantiate(platforms[0], 
                     transform.position + new Vector3(0, (_platformHeight + _platformGap) * i, 0),
                     Quaternion.Euler(0, angle + _angleTurn * i, 0));
                 p.transform.parent = this.transform;
@@ -61,9 +62,49 @@ public class TowerController : MonoBehaviour
                 // }
 
                 #endregion
-                _floors[i][(angle / platform[Random.Range(0, platform.Length)].angle) - 1] = p;
+                _floors[i][(angle / platforms[0].angle) - 1] = p;
 
-                var k = Random.Range(0, (angle / platform[Random.Range(0, platform.Length)].angle) - 1);
+                var k = Random.Range(0, (angle / platforms[0].angle) - 1);
+                _floors[i][k].GetComponent<MeshRenderer>().material = undestroyable;
+                _floors[i][k].gameObject.tag = "Undestroyable";
+            }
+        }
+        
+        for (var i = floorNumbers * 1/3; i < floorNumbers * 2/3; i++)
+        {
+            _floors[i] = new PlatformController[360 / platforms[1].angle];
+        
+            for (int angle = platforms[1].angle; angle <= 360;
+                angle += platforms[1].angle)
+            {
+                PlatformController p = Instantiate(platforms[1], 
+                    transform.position + new Vector3(0, (_platformHeight + _platformGap) * i, 0),
+                    Quaternion.Euler(0, angle + _angleTurn * i, 0));
+                p.transform.parent = this.transform;
+                
+                _floors[i][(angle / platforms[1].angle) - 1] = p;
+        
+                var k = Random.Range(0, (angle / platforms[1].angle) - 1);
+                _floors[i][k].GetComponent<MeshRenderer>().material = undestroyable;
+                _floors[i][k].gameObject.tag = "Undestroyable";
+            }
+        }
+        
+        for (var i = floorNumbers * 2/3; i < floorNumbers; i++)
+        {
+            _floors[i] = new PlatformController[360 / platforms[2].angle];
+        
+            for (int angle = platforms[2].angle; angle <= 360;
+                angle += platforms[2].angle)
+            {
+                PlatformController p = Instantiate(platforms[2], 
+                    transform.position + new Vector3(0, (_platformHeight + _platformGap) * i, 0),
+                    Quaternion.Euler(0, angle + _angleTurn * i, 0));
+                p.transform.parent = this.transform;
+               
+                _floors[i][(angle / platforms[2].angle) - 1] = p;
+        
+                var k = Random.Range(0, (angle / platforms[2].angle) - 1);
                 _floors[i][k].GetComponent<MeshRenderer>().material = undestroyable;
                 _floors[i][k].gameObject.tag = "Undestroyable";
             }
@@ -73,11 +114,11 @@ public class TowerController : MonoBehaviour
     {
         try
         {
-            for (int j = 0; j < (360 / platform[Random.Range(0, platform.Length)].angle); j++)
+            for (var j = 0; j < (360 / platforms[Random.Range(0, platforms.Length)].angle); j++)
             {
                 _floors[_lastFloor][j].GetComponent<PlatformController>().Pop();
             }
-
+            
             _lastFloor--;
         }
         catch (IndexOutOfRangeException)
